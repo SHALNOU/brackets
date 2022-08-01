9dda7f276d4dae385a5c0d94ba78b7e6b01daab7
Анастасия Байбородова <anastasiabajborodova@192.168.0.2>
2022-08-01 20:22:13 +0500

    init:finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+	const bracketsPair = {}
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0]
+	}
+
+	let stack = []
+
+	for (let i = 0; i < str.length; i++) {
+		let currentSymbol = str[i]
+		let topElement = stack[stack.length - 1]
+
+		if (stack.length === 0) {
+			stack.push(currentSymbol)
+		} else {
+			if (bracketsPair[currentSymbol] === topElement) {
+				stack.pop()
+			} else {
+				stack.push(currentSymbol)
+			}
+		}
+	}
+	return stack.length === 0
+};
